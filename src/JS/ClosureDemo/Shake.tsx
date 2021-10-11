05750b48e17f4e757e9eff17c22d349e37d8b3a9
肖明辉 <xiao.xmh@bitsun-inc.com>
2021-10-11 16:19:53 +0800

    资料补充

--- src/JS/ClosureDemo/Shake.tsx
+++ src/JS/ClosureDemo/Shake.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-11 16:00:06
- * @LastEditTime: 2021-10-11 16:01:19
+ * @LastEditTime: 2021-10-11 16:19:45
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \dailyProgress\src\JS\ClosureDemo\Shake.ts
@@ -9,19 +9,5 @@
 import React from 'react';
 
 export default () => {
-  // 防抖
-  function debounce(fn: any, delay = 300) {
-    let timer: any; //闭包引用的外界变量
-    return function () {
-      const args = arguments;
-      if (timer) {
-        clearTimeout(timer);
-      }
-      timer = setTimeout(() => {
-        fn.apply(this, args);
-      }, delay);
-    };
-  }
-
   return <div></div>;
 };
